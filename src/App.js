07937689fc00826b3f1b07f7219207708f293416
AUNT1oN256 <anton.mironets@gmail.com>
2024-12-21 16:24:17 +0200

    Create game "click counter"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <div>Hi, React!</div>;
+  const [count, setCount] = useState(0);
+
+  const increase = () => setCount(count + 1);
+  const decrease = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  if(count < 0) {
+    alert("Error, you cant do this");
+    reset();
+  }
+
+  return(
+  <div style={styles.container}>
+  <h1 style={styles.heading}>Clicker</h1>
+  <p style={styles.counter}>Score: {count}</p>
+  <div>
+    <button style={styles.button} onClick={increase}>+</button>
+    <button style={styles.button} onClick={decrease}>-</button>
+    <button style={styles.button} onClick={reset}>Restar</button>
+  </div>
+  </div>
+)
+}
+
+
+//Simple styles
+const styles = {
+  container: {
+    textAlign: 'center',
+    marginTop: '50px'
+  },
+  heading: {
+    color: '#007bff'
+  },
+  counter: {
+    fontSize: '36px',
+    margin: '20px'
+  },
+  button: {
+    padding: '10px 20px',
+    margin: '5px',
+    fontSize: '16px',
+    border: 'none',
+    boredRadius: '5px',
+    cursor: 'pointer',
+    background: '#007bff',
+    color: '#fff'
+  }
 }
 
 export default App;
